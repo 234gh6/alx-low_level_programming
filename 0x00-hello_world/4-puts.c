b65a3e752d048f8b123c9f12b5a8156b7039a698
234gh6 <oviemukoro951@gmail.com>
2022-07-01 18:03:30 -0700

    ok ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-vagrant@vagrant-ubuntu-trusty-64.1835:1568837518
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
